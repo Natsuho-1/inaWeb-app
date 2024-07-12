feb5e2b93d4f64ebf87a1c301bad7d095e4cea26
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-07-11 23:18:40 -0600

    Cambio en modelos
    Cambio en modelos docente y asignatura

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -29,4 +29,15 @@ class Docente extends Model
     {
         return $this->belongsTo(Persona::class, 'idpersonal');
     }
+
+    public function especialidad()
+    {
+        return $this->belongsTo(Especialidad::class, 'especialidad', 'idespecialidad');
+    }
+
+
+    public function asignaciones()
+    {
+        return $this->hasMany(AsignacionAsignaturas::class, 'iddocente', 'iddocente');
+    }
 }
